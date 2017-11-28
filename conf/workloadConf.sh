6e61a5647abeca7e7993c9ea492ae4e1b7ca6419
BryantChang <finish740@sina.com>
2017-11-28 15:49:08 +0800

    first commit

---
+++ conf/workloadConf.sh
@@ -0,0 +1,12 @@
+1=SVDPlusPlus
+2=PageRank
+# 3=TriangleCount
+# 4=ShortestPath
+# 5=Terasort
+# 6=ConnectedComponent
+# 7=SVM
+# 8=PCA
+# 9=DecisionTree
+# 10=KMeans
+# 11=LogisticRegression
+# 12=MatrixFactorization
