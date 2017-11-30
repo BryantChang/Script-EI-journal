d51f24ab15a13027bc84a422e342d1abdb108340
BryantChang <finish740@sina.com>
2017-11-30 12:10:27 +0800

    modify files

--- scripts_to_run/testScript.sh
+++ scripts_to_run/testScript.sh
@@ -3,13 +3,13 @@
 sed -i "/TYPE=/ c TYPE=300M" /home/hadoop/bryantchang/codes/SparkBench/SVDPlusPlus/conf/env.sh
 sed -i "/SPARK_EXECUTOR_MEMORY=/ c SPARK_EXECUTOR_MEMORY=1460m" /home/hadoop/bryantchang/codes/SparkBench/SVDPlusPlus/conf/env.sh
 sed -i 's///g' /home/hadoop/bryantchang/codes/SparkBench/SVDPlusPlus/conf/env.sh
-sh /home/hadoop/bryantchang/codes/SparkBench/SVDPlusPlus/bin/run.sh WSMC 0&
+sh /home/hadoop/bryantchang/codes/SparkBench/SVDPlusPlus/bin/run.sh WSMC_BIG 0&
 sleep 1
 
 sed -i "/TYPE=/ c TYPE=300M" /home/hadoop/bryantchang/codes/SparkBench/PageRank/conf/env.sh
 sed -i "/SPARK_EXECUTOR_MEMORY=/ c SPARK_EXECUTOR_MEMORY=1600m" /home/hadoop/bryantchang/codes/SparkBench/PageRank/conf/env.sh
 sed -i 's///g' /home/hadoop/bryantchang/codes/SparkBench/PageRank/conf/env.sh
-sh /home/hadoop/bryantchang/codes/SparkBench/PageRank/bin/run.sh WSMC 0&
+sh /home/hadoop/bryantchang/codes/SparkBench/PageRank/bin/run.sh WSMC_BIG 0&
 
 sleep 600
 
