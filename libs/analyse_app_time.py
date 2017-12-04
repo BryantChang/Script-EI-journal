05d5543526417cfe4c3452489fbff041599fd8f8
BryantChang <finish740@sina.com>
2017-12-05 00:06:26 +0800

    modify filse

--- libs/analyse_app_time.py
+++ libs/analyse_app_time.py
@@ -15,6 +15,9 @@ isExists = os.path.exists(summary_log_base)
 time_dic = {'SVDPlusPlus':0, 'PageRank':0, 'ShortestPaths':0, 'PCA':0 ,'SVM':0,
             'Terasort':0, 'ConnectedComponent':0, 'DecisionTree':0,
             'TriangleCount':0, 'MF':0, 'KMeans':0, 'LogisticRegression':0}
+count_dic = {'SVDPlusPlus':0, 'PageRank':0, 'ShortestPaths':0, 'PCA':0 ,'SVM':0,
+             'Terasort':0, 'ConnectedComponent':0, 'DecisionTree':0,
+             'TriangleCount':0, 'MF':0, 'KMeans':0, 'LogisticRegression':0}
 if not isExists:
     os.mkdir(summary_log_base)
 
@@ -27,11 +30,12 @@ for filename in os.listdir(res_log_base + "/" + exp_type):
         app_name = a[index][0]
         sec = int(a[index][1].replace('\n', ''))
         time_dic[app_name] += sec
+        count_dic[app_name] += 1
         index += 1
     handeler.close()
 
 for index in time_dic:
-    time_dic[index] /= 11
+    time_dic[index] /= count_dic[index]
 
 summary_log_handeler = open(summary_log_path, 'w')
 summary_log_handeler.write("APP,Time\n")
